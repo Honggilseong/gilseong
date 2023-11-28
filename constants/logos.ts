a07579a6c223a3df6129d03ef38286e92619a093
unknown <hong.gilseong@gmail.com>
2023-11-28 18:18:10 +0900

    기술스택 변수 작성

---
+++ constants/logos.ts
@@ -0,0 +1,22 @@
+import { nextjs, reactjs, tailwind, typescript } from "@/assets";
+
+export const technologies = [
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+
+  {
+    name: "Next JS",
+    icon: nextjs,
+  },
+];
