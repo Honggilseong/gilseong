7824a42be8a67d2da70da16ffed52ea23c7481ec
unknown <hong.gilseong@gmail.com>
2023-11-28 18:10:33 +0900

    기술 이미지 추가

---
+++ assets/index.ts
@@ -0,0 +1,6 @@
+import tailwind from "./logos/tailwind.png";
+import nextjs from "./logos/nextjs.png";
+import typescript from "./logos/typescript.png";
+import reactjs from "./logos/reactjs.png";
+
+export { tailwind, nextjs, typescript, reactjs };
