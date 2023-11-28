ea19672a7adcd7ff68d375e0e7ff9d7d2f33ccb9
unknown <hong.gilseong@gmail.com>
2023-11-28 18:18:55 +0900

    ball 캔버스 작성

---
+++ app/_components/tech.tsx
@@ -0,0 +1,17 @@
+import BallCanvas from "@/components/canvas/ball";
+
+import { technologies } from "@/constants/logos";
+
+const Tech = () => {
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((tech) => (
+        <div className="w-28 h-28" key={tech.name}>
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Tech;
