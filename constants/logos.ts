31e6cd5a362ce3f7104c70ba618b7eccd7fb64ad
unknown <hong.gilseong@gmail.com>
2023-11-29 11:02:29 +0900

    technologies 변수 값 일부 변경

--- constants/logos.ts
+++ constants/logos.ts
@@ -2,21 +2,20 @@ import { nextjs, reactjs, tailwind, typescript } from "@/assets";
 
 export const technologies = [
   {
-    name: "TypeScript",
-    icon: typescript,
+    name: "Next JS",
+    icon: nextjs,
   },
   {
     name: "React JS",
     icon: reactjs,
   },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
 
   {
     name: "Tailwind CSS",
     icon: tailwind,
-  },
-
-  {
-    name: "Next JS",
-    icon: nextjs,
   },
 ];
